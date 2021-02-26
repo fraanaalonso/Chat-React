d2f86797eeae38fb07a155864bae42fab3debcf7
Fran Alonso <fraloal97@gmail.com>
2021-02-26 09:30:45 +0100

    Both components Channel and Button created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import Button from './components/Button';
+import './index.css'
+import  Channel  from './components/Channel';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyAX_zpDqGSxQnLWGRdCKMk8AM_XSbqOF5o",
+    authDomain: "chat-react-edc32.firebaseapp.com",
+    projectId: "chat-react-edc32",
+    storageBucket: "chat-react-edc32.appspot.com",
+    messagingSenderId: "891470607365",
+    appId: "1:891470607365:web:c8791fd0fe9d58526b586c",
+    measurementId: "G-MGDN3L6V8R"
+});
+const auth = firebase.auth();
+const db = firebase.firestore();
+
 
 function App() {
+
+  const [user, setuser] = useState(()=> auth.currentUser)
+  const [initializing, setinitializing] = useState(true)
+  useEffect(() => {
+    const unsuscribe = auth.onAuthStateChanged( user => {
+      if ( user ){
+        setuser( user );
+      }else{
+        setuser( null );
+      }
+
+      if ( initializing ){
+        setinitializing( false );
+      }
+    })
+
+    return unsuscribe;
+  })
+  const signInWithGoogle =async() => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.useDeviceLanguage();
+
+    try{
+      await auth.signInWithPopup(provider);
+    }catch(error){
+      console.log( error );
+    }
+  };
+
+  const signOut = async() => {
+    try{
+      await firebase.auth().signOut();
+    }catch(error){
+      console.log( error.message );
+    }
+  };
+
+
+  if( initializing ) return 'Loading...';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user 
+      
+      ? 
+      
+      <>
+        <Button  onClick={signOut}>Cerrar Sesión</Button>
+        <Channel user={user} db={db} />
+      </>
+      
+      : 
+      
+      <Button  onClick={signInWithGoogle}>Inicia Sesión con Google</Button>}
     </div>
   );
 }
---
+++ src/components/Channel.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react'
+import firebase from 'firebase/app';
+
+const Channel = ({user=null, db=null}) => {
+    const [messages, setmessages] = useState([]);
+    const [newMessage, setnewMessage] = useState('');
+    const [ uid, displayName, photoURL ] =user
+    useEffect(() => {
+        if( db ){
+            const unsuscribe = db
+                .collection('messages')
+                .orderBy('CreatedAt')
+                .limit(100)
+                .onSnapshot(querySnapshot => {
+                                    const data = querySnapshot.docs.map(doc => ({
+                                        ...doc.data(),
+                                        id: doc.id,
+                                    }))
+
+                                    setmessages(data);
+                                });
+                return unsuscribe
+        }
+    }, [db]);
+
+    const handleOnChange = (e) => {
+        setnewMessage(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if( db ){
+            db.collection('messages').add({
+
+                text: newMessage,
+                CreatedAt: firebase.firestore.FieldValue.serverTimestamp(),
+                uid,
+                displayName,
+                photoURL
+            }
+            )
+        }
+    }
+
+    return (
+    <>
+        <ul>
+            {messages.map( message => (
+                <li key={message.id}>{message.text}</li>
+            ))}
+        </ul>
+
+        <form onSubmit={ handleSubmit }>
+            <input
+                type="text"
+                value={newMessage}
+                onChange={handleOnChange}
+                placeholder="Escribe aquí tu mensaje"
+            />
+
+            <button type="submit" disabled={!newMessage}>Enviar</button>
+
+        </form>
+    </>
+    );
+}
+
+export default Channel;
